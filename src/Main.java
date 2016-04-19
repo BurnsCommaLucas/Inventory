e2cdcded5485adc80cab712cd396768cd863ecfa
BurnsCommaLucas <lburns534@gmail.com>
2016-04-18 22:14:02 -0700

    Wowee it works. Finished IO for cooks

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.HashMap;
+import static java.lang.Character.isDigit;
 
 /**
  * FOR THE LOVE OF GOD RUN THIS ON A MAC, I HAVE NO IDEA IF IT WORKS ON PC
  */
 public class Main {
 
-    public static Integer[] nums;
-    public static String[] names;
+    public static Integer[][] nums;
     public static String[] raws;
     public static String cookPath = "Test/Out/Cooks/"; //     MAKE THIS THE COOK FOLDER
     public static String managePath = "Test/Out/Mgmt/"; //     MAKE THIS THE MANAGEMENT FOLDER
@@ -16,6 +17,9 @@ public class Main {
     public static String cookExt = ".txt";
     public static String fileName;
     public static Database db;
+    public static HashMap<String, String[]> meals;
+    public static String[] headers;
+    public static int tableNumber;
 
     public static void main(String[] args) throws FileNotFoundException {
         SimpleDateFormat ft = new SimpleDateFormat ("yyyy.MM.dd 'at' HH.mm.ss");
@@ -29,11 +33,11 @@ public class Main {
         TimerTask task = new DirWatcher(inPath, "txt") {
                 protected void onChange(File file, String action) {
                 if (action == "add") {
-                    names = new String[] {};
                     raws = new String[] {};
-                    nums = new Integer[] {};
                     Date d = new Date();
                     fileName = ft.format(d);
+                    meals = new HashMap<String, String[]>();
+                    headers = new String[] {};
 
                     // First get input
                     System.out.println("huh");
@@ -41,22 +45,66 @@ public class Main {
                         FileReader fileReader = new FileReader(file);
                         BufferedReader bufferedReader = new BufferedReader(fileReader);
                         String line;
+                        String header = "";
                         while ((line = bufferedReader.readLine()) != null) {
-                            raws = (String[]) addElement(raws, line);
+                            if (line.equals(""))
+                                continue;
+                            char c = line.charAt(0);
+                            if (c == '/')
+                            {
+                                header = line;
+                                meals.put(header, new String[] {});
+                                headers = (String[]) addElement(headers, header);
+                            }
+                            else if (isDigit(c))
+                            {
+                                tableNumber = c;
+                            }
+                            else
+                            {
+                                raws = (String[]) addElement(raws, line);
+                                meals.replace(header, (String[]) addElement(meals.get(header), line));
+                            }
                         }
                         fileReader.close();
                     } catch (IOException e) {
                         e.printStackTrace();
+                    }
+                    nums = new Integer[headers.length][];
+                    for (int i = 0; i < headers.length; i++)
+                    {
+                        nums[i] = new Integer[]{};
                     }
 
                     parse();
                     // Then output
                     try {
                         PrintWriter cookWriter = new PrintWriter(cookPath + fileName + cookExt);
-                        for (int i = 0; i < names.length; i++)
+                        cookWriter.println("" + tableNumber);
+                        for (int i = 0; i < headers.length; i++)
                         {
-                            cookWriter.println(names[i] + " " + nums[i] + " " + db.getStockChar(names[i], nums[i]));
-                            db.updateStock(names[i], db.getStock(names[i]) - nums[i], managePath);
+                            boolean canMake = true;
+                            for (int j = 0; j < meals.get(headers[i]).length; j++)
+                            {
+                                if (db.getStockChar(meals.get(headers[i])[j], nums[i][j]) != 'y')
+                                {
+                                    cookWriter.println(headers[i] + " n");
+                                    canMake = false;
+                                    break;
+                                }
+                            }
+                            if (canMake)
+                            {
+                                cookWriter.println(headers[i] + " y");
+                            }
+
+                            for (int j = 0; j < meals.get(headers[i]).length; j++)
+                            {
+                                String lineTemp = meals.get(headers[i])[j];
+                                db.updateStock(lineTemp, db.getStock(lineTemp) - nums[i][j], managePath);
+                                cookWriter.println(lineTemp + " " + nums[i][j]);
+                            }
+                            cookWriter.println();
                         }
                         cookWriter.close();
                     } catch (FileNotFoundException e) {
@@ -77,23 +125,30 @@ public class Main {
 
     public static void parse()
     {
-        String temp;
-        for (int i = 0; i < raws.length; i++)
+        String lineTemp;
+        // Iterate through headers (MEAL KEYS)
+        for (int i = 0; i < headers.length; i++)
         {
-            temp = raws[i];
-            while (temp.length() > 0)
+            String[] noNumbers = new String[] {};
+            // Iterate through meal requirements
+            for (int j = 0; j < meals.get(headers[i]).length; j++)
             {
-                if (db.includes(temp.substring(0, temp.indexOf(' '))))
+                lineTemp = meals.get(headers[i])[j];
+                while (lineTemp.length() > 0)
                 {
-                    String itWorkedLol = temp.substring(0, temp.indexOf(' '));
-                    names = (String[]) addElement(names, itWorkedLol);
-                    String whatIsHappeneing = temp.replaceAll("[^0-9]", "");
-                    nums = (Integer[]) addElement(nums, new Scanner(whatIsHappeneing).nextInt());
-                    temp = "";
-                    break;
+                    if (db.includes(lineTemp.substring(0, lineTemp.indexOf(' '))))
+                    {
+                        String itWorkedLol = lineTemp.substring(0, lineTemp.indexOf(' '));
+                        noNumbers = (String[]) addElement(noNumbers, itWorkedLol);
+                        String whatIsHappeneing = lineTemp.replaceAll("[^0-9]", "");
+                        nums[i] = (Integer[]) addElement(nums[i], new Scanner(whatIsHappeneing).nextInt());
+                        lineTemp = "";
+                        break;
+                    }
+                    lineTemp = lineTemp.substring(lineTemp.indexOf(' ') + 1);
                 }
-                temp = temp.substring(temp.indexOf(' ') + 1);
             }
+            meals.replace(headers[i], noNumbers);
         }
     }
 
